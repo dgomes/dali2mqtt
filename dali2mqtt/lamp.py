f055938e63c84745a9d74d57df957f7d6567c9e7
Diogo Gomes <diogogomes@gmail.com>
2024-02-03 16:51:04 +0000

    Log err

--- dali2mqtt/lamp.py
+++ dali2mqtt/lamp.py
@@ -47,7 +47,7 @@ class Lamp:
             self.min_physical_level = _min_physical_level.value
         except Exception as err:
             self.min_physical_level = None
-            logger.warning("Set min_physical_level to None as %s doesn't have value attribute.", _min_physical_level)
+            logger.warning("Set min_physical_level to None as %s failed: %s", _min_physical_level, err)
         self.min_level = driver.send(gear.QueryMinLevel(short_address)).value
         self.max_level = driver.send(gear.QueryMaxLevel(short_address)).value
         self.level = driver.send(gear.QueryActualLevel(short_address)).value
